c23af9dcbc20172059b458f0ffd336c7b51d0177
Adrian Soto <78667514+AdrianSotoM@users.noreply.github.com>
2022-02-23 09:03:37 -0600

    Update LMHRSinNHANEScode.R

--- LMHRSinNHANEScode.R
+++ LMHRSinNHANEScode.R
@@ -1,35 +1,43 @@
-library(RNHANES)
-library(dplyr)
+###### SET UP #####
+#Directory set up
+setwd("C:/Users/adria/Dropbox/UIEM/LEAD/Proyectos/LMHR")
 
-## Getting NHANES' data
-### LDL (in mg/dL) is encoded as "LBLDL" and Triglycerides (in mg/dL) as "LBXTR"
-nhanes1 <-  nhanes_load_data("TRIGLY_G", "2011-2012")
+#Package load
+pacman::p_load(dplyr,RNHANES,readr)
 
-### HDL (in mg/dL) is encoded as "LBDHDD"
-nhanes2 <- nhanes_load_data("HDL_G","2011-2012")
+# Getting NHANES' data
+hdl1 <-  nhanes_load_data("HDL_D","2005-2006")
+hdl2 <- nhanes_load_data("HDL_E","2007-2008")
+hdl3 <- nhanes_load_data("HDL_F","2009-2010")
+hdl4 <- nhanes_load_data("HDL_G","2011-2012")
+hdl5 <- nhanes_load_data("HDL_H","2013-2014")
+ldl1 <-  nhanes_load_data("TCHOL_D","2005-2006")
+ldl2 <- nhanes_load_data("TCHOL_E","2007-2008")
+ldl3 <- nhanes_load_data("TCHOL_F","2009-2010")
+ldl4 <- nhanes_load_data("TCHOL_G","2011-2012")
+ldl5 <- nhanes_load_data("TCHOL_H","2013-2014")
+tg1 <-  nhanes_load_data("TRIGLY_D","2005-2006")
+tg2 <- nhanes_load_data("TRIGLY_E","2007-2008")
+tg3 <- nhanes_load_data("TRIGLY_F","2009-2010")
+tg4 <- nhanes_load_data("TRIGLY_G","2011-2012")
+tg5 <- nhanes_load_data("TRIGLY_H","2013-2014")
 
-### BMI (kg/m2) is encoded as "BMXBMI"
-nhanes3 <- nhanes_load_data("BMX_G","2011-2012")
+### Merging datasets
+hdl <- bind_rows(hdl1,hdl2,hdl3,hdl4,hdl5)
+ldl <- bind_rows(ldl1,ldl2,ldl3,ldl4,ldl5)
+tg <- bind_rows(tg1,tg2,tg3,tg4,tg5)
 
-### AGE (in years) is encoded as "RIDAGEYR"
-nhanes4 <- nhanes_load_data("DEMO_G","2011-2012")
-
-### Data years and encoding can be corroborated at: 
-###https://wwwn.cdc.gov/nchs/nhanes/search/default.aspx
-
-## Merging NHANES' data sets and excluding children
-nhanes_lipids <- merge(nhanes1,nhanes2,by="SEQN")
-nhanes_demo <- merge(nhanes3,nhanes4,by="SEQN") %>% filter(RIDAGEYR>17)
-nhanes_data <- merge(nhanes_lipids,nhanes_demo,by="SEQN")
+nhanes <- merge(hdl,ldl,by="SEQN")
+nhanes <- merge(nhanes,tg,by="SEQN")
 
 ### Individual criteria
-sum(nhanes_data$LBDLDL>199, na.rm = TRUE)
-sum(nhanes_data$LBDHDD>79, na.rm = TRUE)
-sum(nhanes_data$LBXTR<71, na.rm = TRUE)
+sum(nhanes$LBDLDL>199, na.rm = TRUE)
+sum(nhanes$LBDHDD>79, na.rm = TRUE)
+sum(nhanes$LBXTR<71, na.rm = TRUE)
 
-### All criteria
-nhanes_data <- nhanes_data %>% filter(LBDLDL>199 & LBDHDD>79 & LBXTR<71)
+### All lipid criteria
+nhanes <- nhanes %>% filter(LBDLDL>199 & LBDHDD>79 & LBXTR<71)
 
-sum(nhanes_data$LBDLDL>199, na.rm = TRUE)
-sum(nhanes_data$LBDHDD>79, na.rm = TRUE)
-sum(nhanes_data$LBXTR<71, na.rm = TRUE)
+sum(nhanes$LBDLDL>199, na.rm = TRUE)
+sum(nhanes$LBDHDD>79, na.rm = TRUE)
+sum(nhanes$LBXTR<71, na.rm = TRUE)
